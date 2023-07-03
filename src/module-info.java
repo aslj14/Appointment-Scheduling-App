c3dd86bc4f804c5beae5021375a9b50709c601bc
aslj14 <ajo2690@wgu.edu>
2023-07-03 16:10:10 -0500

    Add Utility package, add java.sql requirement

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,13 @@
 module main.appointmentschedulingsystem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
-    opens main.appointmentschedulingsystem to javafx.fxml;
-    exports main.appointmentschedulingsystem;
+    exports Controller;
+    opens Controller to javafx.fxml;
+    exports Main;
+    opens Main to javafx.fxml;
+    exports Model;
+    opens Model to javafx.base;
 }
