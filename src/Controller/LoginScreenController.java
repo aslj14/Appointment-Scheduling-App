98c2c631421b3ff909d9356ac730ca8393c27526
aslj14 <ajo2690@wgu.edu>
2023-07-15 18:07:08 -0500

    Implement databaseLoginQuery method in LoginScreenController, add error alert if username and/or password is incorrect on login screen

--- src/Controller/LoginScreenController.java
+++ src/Controller/LoginScreenController.java
@@ -1,11 +1,13 @@
 package Controller;
 
+import Model.Users;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -14,10 +16,14 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static DAO.DatabaseLogin.databaseLoginQuery;
 public class LoginScreenController implements Initializable {
 
     Stage stage;
     Parent scene;
+
+        @FXML
+        private Button loginbutton;
 
         @FXML
         private TextField loginscreenpassword;
@@ -26,14 +32,32 @@ public class LoginScreenController implements Initializable {
         private TextField loginscreenusername;
 
         @FXML
+        private Button resetbutton;
+
+        @FXML
         void onActionDisplayMainAppts(ActionEvent event) throws IOException {
-            stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
-            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("MainAppointmentScreen.fxml"));
-            Scene scene = new Scene(loader.load());
-            stage.setTitle("Appointment Scheduling System");
-            stage.setScene(scene);
-            stage.show();
+
+            try {
+                String userName = loginscreenusername.getText();
+                String userPassword = loginscreenpassword.getText();
+                Users usersResults = databaseLoginQuery(userName, userPassword);
+                if (usersResults != null) {
+                    stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+                    FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("MainAppointmentScreen.fxml"));
+                    Scene scene = new Scene(loader.load());
+                    stage.setTitle("Appointment Scheduling System");
+                    stage.setScene(scene);
+                    stage.show();
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setContentText("Invalid username and/or password combination. Try again!");
+                    alert.showAndWait();
+                }
+            } catch (Exception e) {
+                System.out.println("Error:" + e.getMessage());
+            }
         }
+
 
         @FXML
         void onActionResetFields(ActionEvent event) {
