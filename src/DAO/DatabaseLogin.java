87352de16a46c974b4a7647a8233d49603f674ce
aslj14 <ajo2690@wgu.edu>
2023-07-15 17:39:06 -0500

    Create databaseLoginQuery method

--- src/DAO/DatabaseLogin.java
+++ src/DAO/DatabaseLogin.java
@@ -1,4 +1,34 @@
 package DAO;
 
+import Model.Users;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static Helper.JDBC.connection;
+
 public class DatabaseLogin {
+
+    public static Users databaseLoginQuery(String userName, String userPassword) {
+        try {
+            String sql = "SELECT * FROM users WHERE User_Name=? AND PASSWORD=?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, userName);
+            ps.setString(2, userPassword);
+
+            ResultSet rs = ps.executeQuery();
+            Users usersResult = null;
+            if (rs.next()) {
+                int userID = rs.getInt("User_ID");
+                userName = rs.getString("User_Name");
+                userPassword = rs.getString("Password");
+                usersResult = new Users(userID, userName, userPassword);
+            }
+            return usersResult;
+        } catch (Exception e) {
+            System.out.println("Error:" + e.getMessage());
+        }
+        return null;
+    }
 }
