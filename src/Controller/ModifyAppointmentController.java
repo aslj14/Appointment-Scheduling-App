ccdd1e1875578cc90d2df39a3719ac98cf04566e
aslj14 <ajo2690@wgu.edu>
2023-07-14 19:30:55 -0500

    Add functionality to cancel button on modify appointment screen

--- src/Controller/ModifyAppointmentController.java
+++ src/Controller/ModifyAppointmentController.java
@@ -1,4 +1,79 @@
 package Controller;
 
-public class ModifyAppointmentController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ModifyAppointmentController implements Initializable {
+
+    Stage stage;
+    Parent scene;
+
+    @FXML
+    private ComboBox<?> modapptcontact;
+
+    @FXML
+    private ComboBox<?> modapptcustid;
+
+    @FXML
+    private TextField modapptdesc;
+
+    @FXML
+    private DatePicker modapptenddate;
+
+    @FXML
+    private ComboBox<?> modapptendtime;
+
+    @FXML
+    private TextField modapptid;
+
+    @FXML
+    private TextField modapptlocation;
+
+    @FXML
+    private DatePicker modapptstartdate;
+
+    @FXML
+    private ComboBox<?> modapptstarttime;
+
+    @FXML
+    private TextField modappttitle;
+
+    @FXML
+    private TextField modappttype;
+
+    @FXML
+    private ComboBox<?> modapptuserid;
+
+    @FXML
+    void onActionDisplayMainAppt(ActionEvent event) throws IOException {
+        stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("MainAppointmentScreen.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Appointment Scheduling System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void onActionModAppt(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
