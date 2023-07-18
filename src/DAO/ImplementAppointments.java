9a8c80ac144e580818d15a106a9f53577c9a9b44
aslj14 <ajo2690@wgu.edu>
2023-07-18 10:34:14 -0500

    Finish methods for Implement Appointments class

--- src/DAO/ImplementAppointments.java
+++ src/DAO/ImplementAppointments.java
@@ -7,6 +7,8 @@ import javafx.collections.ObservableList;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -54,21 +56,108 @@ public class ImplementAppointments implements AppointmentsDAO {
 
     @Override
     public Appointments getAppointments(int appointmentsID) {
+
+        try {
+            String sql = "SELECT * FROM appointments where Appointment_ID = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, appointmentsID);
+
+            ResultSet rs = ps.executeQuery();
+            Appointments apptsResults = null;
+            if (rs.next()) {
+                int appointmentID = rs.getInt("Appointment_ID");
+                String appointmentTitle = rs.getString("Title");
+                String appointmentDesc = rs.getString("Description");
+                String appointmentLocation = rs.getString("Location");
+                String appointmentType = rs.getString("Type");
+                LocalDateTime appointmentStartDateTime = rs.getTimestamp("Start").toLocalDateTime();
+                LocalDate appointmentsStartDate = appointmentStartDateTime.toLocalDate();
+                LocalTime appointmentStartTime = appointmentStartDateTime.toLocalTime();
+                LocalDateTime appointmentEndDateTime = rs.getTimestamp("End").toLocalDateTime();
+                LocalDate appointmentEndDate = appointmentEndDateTime.toLocalDate();
+                LocalTime appointmentEndTime = appointmentEndDateTime.toLocalTime();
+                int appointmentsCustomerID = rs.getInt("Customer_ID");
+                int appointmentsUserID = rs.getInt("User_ID");
+                int appointmentsContactID = rs.getInt("Contact_ID");
+                Appointments appointments = new Appointments(appointmentID, appointmentTitle,appointmentDesc,
+                        appointmentLocation, appointmentType, appointmentStartDateTime,
+                        appointmentsStartDate, appointmentStartTime, appointmentEndDateTime,
+                        appointmentEndDate, appointmentEndTime, appointmentsCustomerID,
+                        appointmentsUserID, appointmentsContactID);
+            }
+            return apptsResults;
+        } catch (Exception e) {
+            System.out.println("Error:" + e.getMessage());
+        }
         return null;
     }
 
     @Override
-    public int modifyAppointments(int appointmentsID, String appointmentTitle, String appointmentDesc, String appointmentLocation, String appointmentType, LocalDateTime appointmentStartDateTime, LocalDateTime appointmentEndDateTime, int appointmentsCustomerID, int appointmentsUserID, int appointmentsContactID) {
+    public int modifyAppointments(int appointmentsID, String appointmentTitle, String appointmentDesc,
+                                  String appointmentLocation, String appointmentType,
+                                  LocalDateTime appointmentStartDateTime, LocalDateTime appointmentEndDateTime,
+                                  int appointmentsCustomerID, int appointmentsUserID, int appointmentsContactID) {
+        try {
+            String sql = "UPDATE appointments SET Appointment_ID = ?, Title = ?, Description = ?. Location = ?. +" +
+                    "Type = ?, Start = ?, End = ?, Customer_ID = ?, User_ID = ?, Contact_ID = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, appointmentsID);
+            ps.setString(2, appointmentTitle);
+            ps.setString(3, appointmentDesc);
+            ps.setString(4, appointmentLocation);
+            ps.setString(5, appointmentType);
+            ps.setTimestamp(6, Timestamp.valueOf(appointmentStartDateTime));
+            ps.setTimestamp(7, Timestamp.valueOf(appointmentEndDateTime));
+            ps.setInt(8, appointmentsCustomerID);
+            ps.setInt(9, appointmentsUserID);
+            ps.setInt(10, appointmentsContactID);
+            int rowsAffected = ps.executeUpdate();
+            return rowsAffected;
+        } catch (Exception e) {
+            System.out.println("Error:" + e.getMessage());
+        }
         return 0;
     }
 
     @Override
-    public int deleteAppointments(int appointmentsID, int appointmentsCustomerID, String appointmentType) {
+    public int deleteAppointments(int appointmentsID, int appointmentsCustomerID, String appointmentsType) {
+        try {
+            String sql = "DELETE FROM appointments WHERE Appointment_ID = ?, Customer_ID = ?, Type = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, appointmentsID);
+            ps.setInt(2, appointmentsCustomerID);
+            ps.setString(1, appointmentsType);
+            int rowsAffected = ps.executeUpdate();
+            return rowsAffected;
+        } catch (Exception e) {
+            System.out.println("Error:" + e.getMessage());
+        }
         return 0;
     }
 
     @Override
-    public int addNewAppointment(int appointmentsID, String appointmentTitle, String appointmentDesc, String appointmentLocation, String appointmentType, LocalDateTime appointmentStartDateTime, LocalDateTime appointmentEndDateTime, int appointmentsCustomerID, int appointmentsUserID, int appointmentsContactID) {
+    public int addNewAppointment(int appointmentsID, String appointmentTitle, String appointmentDesc,
+                                 String appointmentLocation, String appointmentType,
+                                 LocalDateTime appointmentStartDateTime, LocalDateTime appointmentEndDateTime,
+                                 int appointmentsCustomerID, int appointmentsUserID, int appointmentsContactID) {
+        try {
+            String sql = "INSERT INTO appointments (Appointment_ID = ?, Title = ?. Description = ?, Location = ?," +
+                    "Type = ?, Start = ?, End = ?, Customer_ID = ?, User_ID = ?, Contact_ID = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, appointmentsID)
+            ps.setString(2, appointmentTitle);
+            ps.setString(3, appointmentDesc);
+            ps.setString(4, appointmentLocation);
+            ps.setString(5, appointmentType);
+            ps.setTimestamp(6, Timestamp.valueOf(appointmentStartDateTime));
+            ps.setTimestamp(7, Timestamp.valueOf(appointmentEndDateTime));
+            ps.setInt(8, appointmentsCustomerID);
+            ps.setInt(9, appointmentsUserID);
+            ps.setInt(10, appointmentsContactID);
+
+        } catch (Exception e) {
+            System.out.println("Error:" + e.getMessage());
+        }
         return 0;
     }
 }
