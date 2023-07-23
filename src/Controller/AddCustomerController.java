b445e8a36b37ce3c81b3c1e6a410484f641a0536
aslj14 <ajo2690@wgu.edu>
2023-07-23 00:58:08 -0500

    Add fxid's to fxml and add skeleton to controller

--- src/Controller/AddCustomerController.java
+++ src/Controller/AddCustomerController.java
@@ -1,4 +1,41 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class AddCustomerController {
+
+    @FXML
+    private TextField addcustaddress;
+
+    @FXML
+    private ComboBox<?> addcustcountry;
+
+    @FXML
+    private TextField addcustid;
+
+    @FXML
+    private TextField addcustname;
+
+    @FXML
+    private TextField addcustphonenumber;
+
+    @FXML
+    private TextField addcustpotalcode;
+
+    @FXML
+    private ComboBox<?> addcuststateprovince;
+
+    @FXML
+    void onActionDisplayMainCustomers(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionSaveNewCustomer(ActionEvent event) {
+
+    }
+
 }
