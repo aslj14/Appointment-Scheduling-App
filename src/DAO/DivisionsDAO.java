27a5f669ffff7de2a259cb92e19aec26b71aafc3
aslj14 <ajo2690@wgu.edu>
2023-07-23 00:20:46 -0500

    Finish Divisions DAO class

--- src/DAO/DivisionsDAO.java
+++ src/DAO/DivisionsDAO.java
@@ -1,10 +1,22 @@
 package DAO;
 
+import Model.Divisions;
 import javafx.collections.ObservableList;
 
 public interface DivisionsDAO {
 
-    public ObservableList<>
+    public ObservableList<Divisions> getAllDivisions();
+
+    public Divisions getDivision(int divisionID);
+
+    public int addNewDivision(String newDivision, int divisionCountryID);
+
+    public int modifyDivision(String currentDivision, int currentDivisionCountryID, int newDivisionCountryID);
+
+    public int modifyDivisionCountry(String currentDivision, int currentDivisionCountryID, int newDivisionCountryID);
+
+    public int deleteCurrentDivision(int currentDivisionID, String currentDivision);
+
 
 
 }
--- src/DAO/ImplementDivisions.java
+++ src/DAO/ImplementDivisions.java
@@ -1,4 +1,48 @@
 package DAO;
 
-public class ImplementDivisions {
+import Model.Divisions;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableSet;
+
+import static Helper.JDBC.connection;
+
+public class ImplementDivisions implements DivisionsDAO {
+
+    ObservableList<Divisions> allDivisions = FXCollections.observableArrayList();
+
+    @Override
+    public ObservableList<Divisions> getAllDivisions() {
+        return null;
+    }
+
+    @Override
+    public Divisions getDivision(int divisionID) {
+        return null;
+    }
+
+    @Override
+    public int addNewDivision(String newDivision, int divisionCountryID) {
+        return 0;
+    }
+
+    @Override
+    public int modifyDivision(String currentDivision, int currentDivisionCountryID, int newDivisionCountryID) {
+        return 0;
+    }
+
+    @Override
+    public int modifyDivisionCountry(String currentDivision, int currentDivisionCountryID, int newDivisionCountryID) {
+        return 0;
+    }
+
+    @Override
+    public int deleteCurrentDivision(int currentDivisionID, String currentDivision) {
+        return 0;
+    }
+
+    ObservableList
+
+
+
 }
