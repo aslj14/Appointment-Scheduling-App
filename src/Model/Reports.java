69626827e202be47df80ba3e866eb06933c0504f
aslj14 <ajo2690@wgu.edu>
2023-12-07 12:06:56 -0600

    Finish reports model

--- src/Model/Reports.java
+++ src/Model/Reports.java
@@ -1,4 +1,45 @@
 package Model;
 
 public class Reports {
+
+    private String appointmentMonth;
+
+    private String appointmentType;
+
+    private int total;
+
+    public Reports(String appointmentMonth, String appointmentType, int total) {
+        this.appointmentMonth = appointmentMonth;
+        this.appointmentType = appointmentType;
+        this.total = total;
+    }
+
+    public String getAppointmentMonth() {
+        return appointmentMonth;
+    }
+
+    public void setAppointmentMonth(String appointmentMonth) {
+        this.appointmentMonth = appointmentMonth;
+    }
+
+    public String getAppointmentType() {
+        return appointmentType;
+    }
+
+    public void setAppointmentType(String appointmentType) {
+        this.appointmentType = appointmentType;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return ("Report: " + appointmentMonth + " " + appointmentType + " " + Integer.toString(total));
+    }
 }
